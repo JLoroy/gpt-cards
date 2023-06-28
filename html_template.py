52810ba598d8c6f3d6e99e9523f02b6f583d306d
jlo <jlo@n-side.com>
2023-06-28 09:31:42 +0200

    themes

--- html_template.py
+++ html_template.py
@@ -13,26 +13,31 @@ css_template = """
     .container.ether {
         color: #000000;
         background-color: #c280e8;
+        background-image: url("app/static/Ether.png")
     }
 
     .container.arcane {
         color: #610059;
         background-color: #00d9ff;
+        background-image: url("app/static/Arcane.png")
     }
 
     .container.chaos {
         color: #d80101;
         background-color: #000000;
+        background-image: url("app/static/Chaos.png")
     }
 
     .container.nature {
         color: #e5ff7b;
         background-color: #208f52;
+        background-image: url("app/static/Nature.png")
     }
 
     .container.spirit {
         color: #ffffff;
         background-color: #656565;
+        background-image: url("app/static/Spirit.png")
     }
 
     .img-container {
